42c62e1c1953fb98b8f995859db9c9037ef40f19
Pavlo Markovskyi <pash199060@gmail.com>
2025-07-17 23:35:23 +0300

    add log

--- src/scripts/removeAllContacts.js
+++ src/scripts/removeAllContacts.js
@@ -2,6 +2,8 @@ import {writeContacts} from "../utils/writeContacts.js"
 
 export const removeAllContacts = async () => {
     await writeContacts([]);
+    console.log("Removed all contacts");
+
 };
 
 removeAllContacts();
