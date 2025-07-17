50a9b51d95d7cc40e687a669fbae53a8c00a9e17
Pavlo Markovskyi <pash199060@gmail.com>
2025-07-17 22:41:51 +0300

    add log

--- src/scripts/removeLastContact.js
+++ src/scripts/removeLastContact.js
@@ -4,10 +4,12 @@ import { writeContacts } from "../utils/writeContacts.js";
 export const removeLastContact = async () => {
     const contacts = await readContacts();
     if (contacts.length === 0) {
+        console.log("No contacts to remove.");
         return;
     }
-    contacts.pop();
+    const removedContact = contacts.pop();
     await writeContacts(contacts);
+    console.log("Removed contact:", removedContact);
 };
 
 removeLastContact();
